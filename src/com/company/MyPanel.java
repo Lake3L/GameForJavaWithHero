1d51fcd1e5817629cfe38a6e0b5c6923a90484e0
Lake3L <75627187+Lake3L@users.noreply.github.com>
2021-02-01 17:05:29 +0300

    Add files via upload

---
+++ src/com/company/MyPanel.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class MyPanel extends JPanel {
+    BufferedImage grass;
+    Hero hero;
+
+    public MyPanel() {
+        setLayout(null);
+        hero = new Hero(0, 0);
+        try {
+            grass = ImageIO.read(new File("Images\\grass.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponents(g);
+        for (int yg = 0; yg < 600; yg += 50) {
+            for (int xg = 0; xg < 800; xg += 50) {
+                g.drawImage(grass, xg, yg, null);
+            }
+        }
+                hero.draw(g);
+    }
+}
